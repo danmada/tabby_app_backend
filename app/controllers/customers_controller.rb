facd42c876a667d46c8aae0e100e682958090217
danmada <danmada@gmail.com>
2021-09-22 13:16:12 -0500

    add signup

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,14 @@
 class CustomersController < ApplicationController
     wrap_parameters format: []
-    skip_before_action :authorize, only: [:create, :show]
-    
+    # skip_before_action :authorize, only: [:create, :show]
+
     def show
         cust = Customer.find_by(id: session[:customer_id])
         render json: cust
     end
 
     def create
+        byebug
         cust = Customer.create(cust_params)
         render json: cust, status: :created
     end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,9 @@ class Customer < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :age, presence: true, numericality: { only_integer: true, greater_than: 20 }
+    validates :email, presence: true, uniqueness: true
+    validates :credit_card, presence: true, numericality: true
+
 end
